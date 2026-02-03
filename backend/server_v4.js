30c5bcc42a83a350e047892bc106f8f91a6504b7
Siraj Adil <sirajadil44@gmail.com>
2026-02-03 20:15:05 +0530

    Added a console log to know the backend port

--- backend/server_v4.js
+++ backend/server_v4.js
@@ -727,4 +727,4 @@ function verifyToken(req, resp, next) {
     }
 }
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
