721d91312f1ffff53fa29abb1c3e9e7f8b78972a
Siraj Adil <sirajadil44@gmail.com>
2026-02-03 20:38:08 +0530

    Backend add log for environment

--- backend/server_v4.js
+++ backend/server_v4.js
@@ -727,4 +727,8 @@ function verifyToken(req, resp, next) {
     }
 }
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+    console.log(
+        `Server running on port ${PORT} & Environment = ${process.env.NODE_ENV}`
+    )
+);
